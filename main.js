f9a5773adbbec150832a6d514c83bc52c3fd822f
ferid <tu82v0w4x@code.edu.az>
2023-10-19 00:22:40 +0400

    aaa

--- main.js
+++ main.js
@@ -18,7 +18,7 @@
 
 //1
 
-class Maqaza {
+class Kitablar {
     constructor(ad, muellif, tarix,qiymet,) {
       this.ad = ad;
       this.muellif = muellif;
@@ -27,18 +27,17 @@ class Maqaza {
       
     }
     qiymet_iste(endirim) {
-      this.endirim=endirim
-      this.endirim=(this.qiymet*endirim)/100
+      return endirim=(this.qiymet*endirim)/100
     }
   }
   
   
   
-  const myClass = new Maqaza("harry poter", "J. K. Rowling", '30 Haziran 1997 ', 30);
-  myClass.qiymet_iste(70);
+  const myClass = new Kitablar("harry poter", "J. K. Rowling", '30 Haziran 1997 ', 30);
+  // console.log(myClass.qiymet_iste(70));;
 
- console.log(myClass.ad,myClass.muellif,myClass.tarix,myClass.qiymet,'$','endirimli qiymeti',myClass.endirim,'$',);
-
+ console.log(myClass.ad,myClass.muellif,myClass.tarix,myClass.qiymet,
+  '$','endirimli qiymeti',myClass.qiymet_iste(70),'$',);
 
 
 //2
@@ -52,15 +51,14 @@ class Maqaza {
 //     this.endirimli_qiymet=endirimli_qiymet
 //   }
 //   show(endirim) {
-//     this.endirim=endirim
-//     this.endirimli_qiymet=(this.qiymet*endirim)/100
-//     return this.ad+this.muellif+this.tarix+this.qiymet+"$ "+'tetbiq edilen endirim '+endirim+"% "+'('+'endirimli qiymeti '+this.endirimli_qiymet+"$ "+')'
+//     this.endirim=(this.qiymet*endirim)/100
+//     return this.ad+this.muellif+this.tarix+this.qiymet+"$ "+'tetbiq edilen endirim '+endirim+"% "+'('+'endirimli qiymeti '+this.endirim+"$ "+')'
 //   }
 // }
 
 
 
-// const myClass = new Maqaza("harry poter", "J. K. Rowling", '30 Haziran 1997 ', 30);
+// const myClass = new Maqaza("harry poter ", "J. K. Rowling ", '30 Haziran 1997 ', 30);
 // console.log(myClass.show(70));
 
 
